ed5271033f296d73071a227696a185275896c492
Zach Yang <songyuanoung@gmail.com>
2021-01-04 21:26:21 -0600

    Navigation using hero widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,98 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
-import 'package:sqflite/sqflite.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+void main() => runApp(HeroApp());
 
-  final Future<Database> database = openDatabase(
-    join(await getDatabasesPath(), 'doggie_database_db'),
-    onCreate: (db, version) {
-      return db.execute(
-        "CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)",
-      );
-    },
-    version: 1,
-  );
-
-  Future<void> insertDog(Dog dog) async {
-    final Database db = await database;
-
-    db.insert('dogs', dog.toMap(),
-        conflictAlgorithm: ConflictAlgorithm.replace);
-  }
-
-  Future<List<Dog>> dogs() async {
-    final Database db = await database;
-
-    final List<Map<String, dynamic>> maps = await db.query('dogs');
-
-    return List.generate(maps.length, (index) {
-      return Dog(
-        id: maps[index]['id'],
-        name: maps[index]['name'],
-        age: maps[index]['age'],
-      );
-    });
-  }
-
-  Future<void> updateDog(Dog dog) async {
-    final db = await database;
-
-    await db.update(
-      'dogs',
-      dog.toMap(),
-      where: "id = ?",
-      whereArgs: [dog.id],
+class HeroApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Transition demo',
+      home: MainScreen(),
     );
   }
-
-  Future<void> deleteDog(Dog dog) async {
-    final db = await database;
-
-    await db.delete('dogs', where: "id = ?", whereArgs: [dog.id]);
-  }
-
-  var fido = Dog(
-    id: 0,
-    name: 'Fido',
-    age: 10,
-  );
-
-  await insertDog(fido);
-
-  print(await dogs());
-
-  fido = Dog(
-    id: fido.id,
-    name: fido.name,
-    age: fido.age + 7,
-  );
-
-  await updateDog(fido);
-  print(await dogs());
-
-  await deleteDog(fido);
-  print(await dogs());
 }
 
-class Dog {
-  final int id;
-  final String name;
-  final int age;
-
-  Dog({this.id, this.name, this.age});
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'age': age,
-    };
-  }
-
+class MainScreen extends StatelessWidget {
   @override
-  String toString() {
-    return 'Dog{id: $id, name: $name, age: $age}';
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Main screen'),
+      ),
+      body: GestureDetector(
+          onTap: () {
+            Navigator.push(context, MaterialPageRoute(builder: (_) {
+              return DetailsScreen();
+            }));
+          },
+          child: Hero(
+            tag: "imageHero",
+            child: Image.network('https://picsum.photos/250?image=9'),
+          )),
+    );
+  }
+}
+
+class DetailsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('details screen'),
+      ),
+      body: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Center(
+            child: Hero(
+              tag: 'imageeor',
+              child: Image.network(""
+                  "https://picsum.photos/250?image=9"),
+            ),
+          )),
+    );
   }
 }
